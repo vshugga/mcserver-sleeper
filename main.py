3458a92b508dfcf19053957cdb840a49020fc14b
spencer <spencer@verhoff.net>
2024-03-23 05:40:45 +0000

    initial

---
+++ main.py
@@ -0,0 +1,31 @@
+from mcstatus import JavaServer
+import time
+import subprocess
+
+
+server = JavaServer("localhost", 25565)
+suspend_time = 30
+counter = 0
+inc = 5
+
+while True:
+	time.sleep(inc)
+
+	t1 = time.time()
+	counter += inc
+	status = server.status()
+	num_players = status.players.online
+	print(f"{num_players} online. sleeping in {suspend_time - counter}")
+	if num_players > 0:
+		counter = 0
+
+	if counter > suspend_time and num_players < 1:
+		counter = 0
+		subprocess.run(['sudo', 'systemctl', 'suspend'], check=True)
+		continue
+
+	t2 = time.time()
+	counter -= (t2 - t1)
+
+#print(f"{status.players.online} players online")
+
